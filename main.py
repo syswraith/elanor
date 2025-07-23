4d837c15fe1a9fa79b2bc2b0e0bd7ff51b1e443a
syswraith <syswraith@proton.me>
2025-07-23 22:12:03 +0530

    fouc fix

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import markdown
 from pathlib import Path
 from assets import config as config_module
 
-
 def wikilink_url_builder(label, base, end):
     if label.endswith('.md'):
         label = label[:-3] + '.html'
@@ -26,35 +25,37 @@ for file in os.listdir('./content'):
             text = input_file.read()
 
         html_body = markdown.markdown(
-                text,
-                extensions=[
-                    'fenced_code',
-                    'codehilite',
-                    'markdown.extensions.tables',
-                    'markdown.extensions.meta',
-                    'markdown.extensions.wikilinks'
-                    ],
-                extension_configs={
-                    'markdown.extensions.wikilinks': {
-                        'base_url': '',
-                        'end_url': '',
-                        'build_url': wikilink_url_builder
-                        }
-                    }
-                )
+            text,
+            extensions=[
+                'fenced_code',
+                'codehilite',
+                'markdown.extensions.tables',
+                'markdown.extensions.meta',
+                'markdown.extensions.wikilinks'
+            ],
+            extension_configs={
+                'markdown.extensions.wikilinks': {
+                    'base_url': '',
+                    'end_url': '',
+                    'build_url': wikilink_url_builder
+                }
+            }
+        )
 
         html = f'''<html>
-                <head>
-                  <meta name="viewport" content="width=device-width, initial-scale=1">
-                  <link rel="stylesheet" href="pygments.css" type="text/css">
-                  <link rel="stylesheet" href="https://unpkg.com/normalize.css@8.0.1/normalize.css" type="text/css">
-                  <link rel="stylesheet" href="{config['theme']}" type="text/css" media="print" onload="this.media='all'">
-                  <link rel="icon" type="image/x-icon" href="favicon.ico">
-                </head>
-                <body>
-                {html_body}
-                </body>
-                </html>'''
+            <head>
+              <style>body{{visibility:hidden}}</style>
+              <meta name="viewport" content="width=device-width, initial-scale=1">
+              <link rel="stylesheet" href="pygments.css" type="text/css">
+              <link rel="stylesheet" href="https://unpkg.com/normalize.css@8.0.1/normalize.css" type="text/css">
+              <link rel="stylesheet" href="{config['theme']}" type="text/css" media="print" onload="this.media='all'">
+              <link rel="icon" type="image/x-icon" href="favicon.ico">
+            </head>
+            <body>
+            {html_body}
+            <script>window.addEventListener("DOMContentLoaded",()=>{{document.body.style.visibility="visible"}})</script>
+            </body>
+            </html>'''
 
         with open(f"./generated/{file[:-3]}.html", "w", encoding="utf-8") as f:
             f.write(html)
